e6dce9713bdcd9148c2f22646f0c6f0d4c16a94f
niger <niger@prequel.co>
2026-05-11 15:14:40 -0400

    cool

--- test_secret.sql
+++
@@ -1,4 +0,0 @@
-LOAD './build/release/extension/duckdb_delta_sharing/duckdb_delta_sharing.duckdb_extension';
-LOAD httpfs;
-CREATE SECRET test_secret (TYPE delta_sharing, PROVIDER config, ENDPOINT 'mock', BEARER_TOKEN 'mock');
-SELECT * FROM delta_share_list();
