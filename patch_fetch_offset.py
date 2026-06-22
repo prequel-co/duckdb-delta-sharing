22109b0730bbe27e601e27c0ae858264f006cdc3
niger-prequel <103965718+niger-prequel@users.noreply.github.com>
2026-06-22 09:19:40 -0400

    feat: Add support for wasm and odbc builds (#20)
    * support wasm
    
    * up version
    
    * enable wasm in ci
    
    * try this
    
    * ok
    
    * change osx odbc
    
    * try this
    
    * ok
    
    * try this
    
    * another one
    
    * try this
    
    * maybe it will be loaded now?
    
    * yolo
    
    * fix syntax
    
    * ok
    
    * almost there
    
    * more fixes
    
    * try again
    
    * okey
    
    * try this
    
    * Fix EXC_BAD_ACCESS during asynchronous cancellation by adding thread safety guards to duckdb-odbc
    
    * okey
    
    * once again
    
    * we're trying this
    
    * try this
    
    * more memory leaks
    
    * fix deadlock
    
    * ok
    
    * everybody cleanup
    
    * try fixed commit
    
    * actually add the windows odbc
    
    * okey
    
    * keep going
    
    * sure
    
    * ok

---
+++ patch_fetch_offset.py
@@ -0,0 +1,56 @@
+import os
+
+'''
+SUMMARY:
+This patch script fixes a massive buffer overflow during bulk ODBC fetch calls. 
+DuckDB previously used the absolute chunk index (`row_idx`) instead of the relative fetch index (`rows_fetched`) when populating the host application's bound buffers (e.g. `StrLen_or_IndPtr`).
+This properly re-maps all pointer offsets to use the relative `rows_fetched` index.
+'''
+
+def patch_fetch_offset():
+    f_fetch = 'src/odbc_driver/common/odbc_fetch.cpp'
+    with open(f_fetch, 'r') as f:
+        c = f.read()
+
+    # 1. Patch ColumnWise Loop
+    old_col_loop = """\tfor (SQLULEN row_idx = first_row_to_fetch; row_idx < last_row_to_fetch; ++row_idx) {
+\t\t++chunk_row;
+\t\tSetRowStatus(row_idx, SQL_SUCCESS);"""
+
+    new_col_loop = """\tSQLULEN rows_fetched = 0;
+\tfor (SQLULEN row_idx = first_row_to_fetch; row_idx < last_row_to_fetch; ++row_idx, ++rows_fetched) {
+\t\t++chunk_row;
+\t\tSetRowStatus(rows_fetched, SQL_SUCCESS);"""
+
+    if old_col_loop in c:
+        c = c.replace(old_col_loop, new_col_loop)
+
+    # 2. Patch RowWise Loop
+    old_row_loop = """\tSQLULEN rows_fetched = 0;
+\tfor (SQLULEN row_idx = first_row_to_fetch; row_idx < last_row_to_fetch; ++row_idx, ++rows_fetched) {
+\t\t++chunk_row;
+\t\tSetRowStatus(row_idx, SQL_SUCCESS);
+\t\tauto row_offset = row_size * row_idx;"""
+
+    new_row_loop = """\tSQLULEN rows_fetched = 0;
+\tfor (SQLULEN row_idx = first_row_to_fetch; row_idx < last_row_to_fetch; ++row_idx, ++rows_fetched) {
+\t\t++chunk_row;
+\t\tSetRowStatus(rows_fetched, SQL_SUCCESS);
+\t\tauto row_offset = row_size * rows_fetched;"""
+
+    if old_row_loop in c:
+        c = c.replace(old_row_loop, new_row_loop)
+
+    # 3. Patch pointer offsets
+    c = c.replace("target_val_addr = (uint8_t *)target_val_addr + (row_idx * pointer_size);", "target_val_addr = (uint8_t *)target_val_addr + (rows_fetched * pointer_size);")
+    c = c.replace("target_len_addr += row_idx;", "target_len_addr += rows_fetched;")
+
+    # 4. Patch SetRowStatus error calls
+    c = c.replace("SetRowStatus(row_idx, SQL_ROW_ERROR);", "SetRowStatus(rows_fetched, SQL_ROW_ERROR);")
+
+    with open(f_fetch, 'w') as f:
+        f.write(c)
+    print("Patched odbc_fetch.cpp")
+
+if __name__ == '__main__':
+    patch_fetch_offset()
